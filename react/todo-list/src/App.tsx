e9d995bd045406233969b7982276912c89c3b0e5
Luiz Henrique Camilo <luiz.h.camilo01@hotmail.com>
2025-06-06 19:20:13 -0300

    aula 30

--- react/todo-list/src/App.tsx
+++ react/todo-list/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Header } from "./components/Header/Header";
 import "./styles/global.css";
 import { Tasks } from "./components/Tasks/Tasks";
@@ -13,3 +12,9 @@ function App() {
 }
 
 export default App;
+
+/* o useEffect é um hook do React que permite executar efeitos colaterais em componentes funcionais.
+   Ele é chamado após a renderização do componente e pode ser usado para realizar operações como buscar dados, manipular o DOM ou configurar assinaturas. O useEffect aceita uma função de efeito e um array de dependências, que determina quando o efeito deve ser executado novamente. Se o array de dependências estiver vazio, o efeito será executado apenas uma vez, após a montagem do componente.
+   O useEffect é uma alternativa ao ciclo de vida componentDidMount, componentDidUpdate e componentWillUnmount em componentes de classe.
+   dispara sempre que uma variavel do array de dependence for alterado 
+   Por padrão, sempre será disparadp após a montagem do componente (componentDidMount) */
--- react/todo-list/src/components/Tasks/Tasks.tsx
+++ react/todo-list/src/components/Tasks/Tasks.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 interface Task {
@@ -8,7 +8,7 @@ interface Task {
 }
 
 export const Tasks: React.FC = () => {
-  const [taskTitle, setTasksTitle] = useState("");
+  const [taskTitle, setTaskTitle] = useState("");
   const [tasks, setTasks] = useState([] as Task[]);
 
   // função para lidar com o envio do formulário
@@ -22,13 +22,27 @@ export const Tasks: React.FC = () => {
       return;
     }
     // logica para adicionar a tarefa
-    setTasks([
+    const newTask = [
       ...tasks,
       { id: new Date().getTime().toString(), title: taskTitle, done: false },
-    ]);
-    setTasksTitle("");
+    ];
+    setTasks(newTask);
+    // ...tasks é usado para manter as tarefas existentes e adicionar a nova tarefa
+    localStorage.setItem("tasks", JSON.stringify(newTask));
+    // localStorage.setItem é usado para armazenar as tarefas no armazenamento local do navegador
+    // JSON.stringify(tasks) é usado para converter o array de tarefas em uma string JSON
+    setTaskTitle("");
     // setTasksTitle("") é usado para limpar o campo de entrada após a tarefa ser adicionada
   }
+  useEffect(() => {
+    const tasksOnLocalStorage = localStorage.getItem("tasks");
+    // localStorage.getItem é usado para obter as tarefas armazenadas no armazenamento local do navegador
+    if (tasksOnLocalStorage) {
+      setTasks(JSON.parse(tasksOnLocalStorage));
+      // JSON.parse é usado para converter a string JSON de volta em um array de tarefas
+    }
+  }, []);
+
   return (
     <section className={styles.container}>
       <form onSubmit={handleSubmitTask}>
@@ -38,7 +52,7 @@ export const Tasks: React.FC = () => {
           <label htmlFor="task-title">Adcionar Tarefa</label>
           <input
             value={taskTitle}
-            onChange={(e) => setTasksTitle(e.target.value)}
+            onChange={(e) => setTaskTitle(e.target.value)}
             // onCchange mudança de valor do input
             id="task-title"
             type="text"
